142b08ceb4631606a4dda06dd4135dbf6aa29592
yuelin wen <tyleryuelinwen@gmail.com>
2023-06-29 18:46:39 -0400

    fix bug

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google'
-import { connectToDB } from "@utls/database";
+import { connectToDB } from "@utils/database";
 import User from "@models/User";
 
 const handler = NextAuth({
