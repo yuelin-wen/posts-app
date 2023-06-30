a3624456d83abfe925a29746a85c49e2b6a76680
yuelin wen <tyleryuelinwen@gmail.com>
2023-06-30 16:52:46 -0400

    myprofile page added

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,8 +5,11 @@ import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
-const PromptCard = (post, handleTagClick, handleEdit, handleDelete) => {
+const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
   const [copied, setCopied] = useState("");
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
 
   const handleCopy = () => {
     setCopied(post.prompt);
@@ -20,19 +23,19 @@ const PromptCard = (post, handleTagClick, handleEdit, handleDelete) => {
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
         <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
-          {/* <Image
+        <Image
             src={post.creator.image}
-            alt="user_image"
+            alt='user_image'
             width={40}
             height={40}
-            className="rounded-full object0contain"
-          /> */}
-          {/* <div className="flex flex-col">
+            className='rounded-full object-contain'
+          />
+          <div className="flex flex-col">
             <h3 className="font-satoshi font-semibold text-gray-900">
               {post.creator.username}
             </h3>
             <p>{post.creator.email}</p>
-          </div> */}
+          </div>
         </div>
         <div className="copy_btn" onClick={handleCopy}>
           <Image
@@ -53,6 +56,22 @@ const PromptCard = (post, handleTagClick, handleEdit, handleDelete) => {
       >
         {post.tag}
       </p>
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
